8e0b3699d3c0ef409b53771c912b956c886c3d2d
ckhemani <chandru.khemani@gmail.com>
2022-04-02 15:54:06 -0400

    Staged all classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import interfaces.Shape;
+import shapes.Circle;
+import shapes.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        //Coding to an Interface and not the implementation
+        Shape shape = new Circle(10);
+
+        //Drawing the shape
+        shape.draw();
+        System.out.println("Area of Circle " + shape.getArea());
+
+        //Switching from one implementation to another easily
+        shape = new Rectangle(10,10);
+        shape.draw();
+        System.out.println( "Area of Rectangle = " + shape.getArea());
+
+        //Default in the Interface
+        shape.defaultTest();
     }
 }
